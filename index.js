593456248b35ef5e8d97c0490e8fe91bdd8187e8
uraniumrainbow <uraniumrainbow@gmail.com>
2023-11-14 11:06:38 -0700

    table of contents

--- index.js
+++ index.js
@@ -3,3 +3,112 @@ const inquirer = require('inquirer');
 const connection = require('./config/connection');
 const validate = require('./validate');
 require('console.table');
+
+require('dotenv').config
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'employee_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+});
+
+//intro ascii image
+afterConnection = () => {
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*        EMPLOYEE MANAGER         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+    userChoices();
+};
+
+const userChoices = () => {
+    inquirer
+        .createPromptModule([
+            {
+                type: 'list',
+                name: 'choices',
+                message: 'What would you like to do?',
+                choices: ['View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role',
+                'Update an employee manager',
+                "View employees by department",
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'View department budgets',
+                'No Action']
+            }
+        ])
+            .then((answers) => {
+                const { choices } = answers;
+
+                if (choices === 'View all departments') {
+                    showDepartments();
+                }
+
+                if (choices === 'View all roles') {
+                    showRoles();
+                }
+
+                if (choices === 'View all employees') {
+                    showEmployees();
+                }
+
+                if (choices === 'Add a department') {
+                    addDepartment();
+                }
+
+                if (choices === 'Add a role') {
+                    addRole();
+                }
+
+                if (choices === 'Add an employee') {
+                    addEmployee();
+                }
+
+                if (choices === 'Update an employee role') {
+                    updateEmployee();
+                }
+
+                if (choices === 'Update an employee manager') {
+                    updateManager();
+                }
+
+                if (choices === 'View employees by department') {
+                    employeeDepartment();
+                }
+
+                if (choices === 'Delete a department') {
+                    deleteDepartment();
+                }
+
+                if (choices === 'Delete a role') {
+                    deleteRole();
+                }
+
+                if (choices === 'Delete an employee') {
+                    deleteEmployee();
+                }
+
+                if (choices === 'View department budgets') {
+                    viewBudgets();
+                }
+
+                if (choices === "No Action") {
+                    connection.end()
+                };
+            });
+};
